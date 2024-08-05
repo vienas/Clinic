cd78fd9f767bd677e8be56bdb6281dc595d2cc20
Vienas <67314023+vienas@users.noreply.github.com>
2024-08-05 23:28:03 +0200

    change edit page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                         
                         @auth
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('procedure.index') }}">Edytuj Zabiegi</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('patient.edit') }}">Lista Pacjentów</a></li>
+                        <!-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('patient.edit') }}">Lista Pacjentów</a></li> -->
                         <!-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('profile.edit') }}" style="color: lightgreen">Edytuj profil</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('logout') }}" style="color: lightgreen">Wyloguj</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" style="color: silver">Witaj, {{ Auth::user()->name }}</a></li> -->
--- resources/views/patient/edit.blade.php
+++ resources/views/patient/edit.blade.php
@@ -5,60 +5,41 @@
 <!-- Contact Section-->
 <section class="page-section" id="contact">
     <div class="container">
-        <!-- Contact Section Heading-->
-        <h2 class="masthead page-section-heading text-center text-uppercase text-secondary mb-0" style="padding-top: 5rem;">E-rejestracja</h2>
 
-        <!-- Contact Section Form-->
+        <h2 class="masthead page-section-heading text-center text-uppercase text-secondary mb-0" style="padding-top: 5rem;">Edytujesz pacjenta {{ $klinika->name }}</h2>
+
         <div class="row justify-content-center">
             <div class="col-lg-8 col-xl-7">
-                <form action="{{ route('patient.store') }}" method="POST" >
+                <form action="{{ route('patient.update', ['id' => $klinika->id]) }}" method="POST" id="contactForm" name="sentMessage" >
                     {{ csrf_field() }}
-
+                    
+                    @method('PUT')
                     <div class="form-floating mb-3">
-                        <input class="form-control" id="name" name="name" type="text" placeholder="Imię i Nazwisko" maxlength="30" required />
+                        <input value="{{ $klinika->name }}" class="form-control" id="name" name="name" type="text" placeholder="Imię i Nazwisko" data-sb-validations="required" />
                         <label for="number">Imię i Nazwisko</label>
+                        <div class="invalid-feedback" data-sb-feedback="number:required">Imię i Nazwisko</div>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input class="form-control" id="date" name="date" type="text" placeholder="Data wizyty" pattern="\d{4}-\d{2}-\d{2}" maxlength="10" required />
+                        <input value="{{ $klinika->date }}" class="form-control" id="date" name="date" type="text" placeholder="Data wizyty" data-sb-validations="required" />
                         <label for="date">Data wizyty</label>
-                        <div class="invalid-feedback">
-                            Wprowadź poprawną datę wizyty.
-                        </div>
-                    </div>
-
-                    <!-- jQuery UI Datepicker -->
-                    <script>
-                    $(function() {
-                        $("#date").datepicker({
-                            dateFormat: 'yy-mm-dd',
-                            minDate: 0, 
-                            onSelect: function(dateText, inst) {
-                                $(this).val(dateText);
-                            }
-                        });
-                    });
-                    </script>
-
-
-                    <div class="form-floating mb-3">
-                        <input class="form-control" id="phone" name="phone" type="tel" placeholder="Numer telefonu" minlength="9" maxlength="9" pattern="\d{9}" required />
-                        <label for="phone">Numer telefonu</label>
-                        <div class="invalid-feedback">
-                            Wprowadź poprawny numer telefonu.
-                        </div>
+                        <div class="invalid-feedback" data-sb-feedback="date:required">Data wizyty</div>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input class="form-control" id="mail" name="mail" type="email" placeholder="Adres Email" maxlength="30" required/>
-                        <label for="mail">Adres email</label>
-                        <div class="invalid-feedback">
-                            Wprowadź poprawny adres email.
-                        </div>
+                        <input value="{{ $klinika->phone }}" class="form-control" id="phone" name="phone" type="number" placeholder="Numer telefonu" />
+                        <label for="date">Numer telefonu (opcjonalnie)</label>
+                        <div class="invalid-feedback" data-sb-feedback="phone">Numer telefonu</div>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <select class="form-control" id="doctor" name="doctor" placeholder="doctor" required >
+                        <input value="{{ $klinika->mail }}" class="form-control" id="mail" name="mail" type="text" placeholder="Adres Email"  />
+                        <label for="mail">Adres Email (opcjonalnie)</label>
+                        <div class="invalid-feedback" data-sb-feedback="mail:required">An email is required.</div>
+                        <!-- <div class="invalid-feedback" data-sb-feedback="mail:mail">Email is not valid.</div> -->
+                    
+                    <div class="form-floating mb-3">
+                        <select value="{{ $klinika->doctor }}" class="form-control" id="doctor" name="doctor" placeholder="doctor" data-sb-validations="required" >
                             <option value="" disabled selected></option>
                             <option value="lek. Tadeusz Asnyk">lek. Tadeusz Asnyk</option>
                             <option value="lek. Anna Zawada">lek. Anna Zawada</option>
@@ -67,14 +48,14 @@
                         </select>
                         
                         <label for="doctor">Wybór Lekarza</label>
-                        </div>
+                        <div class="invalid-feedback" data-sb-feedback="doctor:required">Wybór Lekarza</div>
+                    </div>
                         
                         <div id="success"></div>
                         <div class="form-group">
-                        <button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Wyślij formularz</button>
+                        <button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Zapisz listę</button>
                     </div>
                 </form>
-
             </div>
         </div>
     </div>
