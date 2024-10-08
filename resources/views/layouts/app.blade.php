e0dc386f0d8912d72ba304d9cb4fd3140673d633
Vienas <67314023+vienas@users.noreply.github.com>
2024-10-08 14:53:55 +0200

    comment "copyright section"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,7 @@
                 </div>
             </div>
         </footer>
-        Copyright Section
+        <!-- Copyright Section-->
         <div class="copyright py-4 text-center text-white">
             <div class="container"><small>Copyright &copy; Your Website 2024</small></div>
         </div>
