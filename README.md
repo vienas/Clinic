458048130adab13352fb3919c4943a784c2283e9
Vienas <67314023+vienas@users.noreply.github.com>
2024-11-27 08:57:26 +0100

    dusk test test_check_if_user_can_add_double_reservation

--- README.md
+++ README.md
@@ -3,16 +3,47 @@
 ## Requirements
 PHP 8.3 x
 MySQL 8.0 x
+or
+Docker Desktop
 
-## Installation
+## Installation without Docker
 ````
 php artisan migrate
 php artisan db:seed
 php artisan storage:link
-
+php artisan dusk:install
+php artisan dusk:chrome-driver
+php artisan serve
+````
 
 ## Usefull commands
 ````
 php artisan db:seed --class=ProcedureCategoriesSeeder
 php artisan db:seed --class=ProceduresSeeder
+````
+
+## Run Test
+````
+php artisan dusk
+````
+
+## Installation with Docker
+````
+./vendor/bin/sail up
+./vendor/bin/sail artisan migrate
+./vendor/bin/sail artisan db:seed
+./vendor/bin/sail artisan storage:link
+./vendor/bin/sail artisan dusk:install
+./vendor/bin/sail artisan dusk:chrome-driver
+````
+
+## Usefull commands
+````
+./vendor/bin/sail artisan db:seed --class=ProcedureCategoriesSeeder
+./vendor/bin/sail artisan db:seed --class=ProceduresSeeder
+````
+
+## Run Test
+````
+./vendor/bin/sail artisan dusk
 ````
--- tests/Browser/AddReservationTest.php
+++ tests/Browser/AddReservationTest.php
@@ -14,16 +14,40 @@ class AddReservationTest extends DuskTestCase
     /**
      * A basic browser test example.
      */
-    public function test_basic_example(): void
-    {
-        // $user = User::factory()->create([
-        //     'email' => 'taylor@laravel.com',
-        // ]);
+    public function test_check_if_user_can_add_double_reservation(): void
+    {   
+        $user = User::factory(5)->create();
+        $this->assertDatabaseEmpty('clinic');
  
         $this->browse(function (Browser $browser) {
             $browser->visit('/pacjent/dodaj')
             ->assertPathIs('/pacjent/dodaj')
-            ->assertSee('E-REJESTRACJA');
+            ->assertSee('E-REJESTRACJA')
+            ->type('name', 'Jan Kowalski')
+            ->click('input[name="date"]')
+            ->click('.ui-datepicker-today')
+            ->type('phone', '777555999')
+            ->type('mail', 'jan@kowalski.pl')
+            ->click('#doctor-select')
+            ->click('#doctor-select option[value="5"]')
+            ->click('#sendMessageButton')
+            ->assertSee('Twoja wizyta została zarejestrowana, dziękujemy!');
+
+            $browser->visit('/pacjent/dodaj')
+            ->assertPathIs('/pacjent/dodaj')
+            ->assertSee('E-REJESTRACJA')
+            ->type('name', 'Jan Kowalski')
+            ->click('input[name="date"]')
+            ->click('.ui-datepicker-today')
+            ->type('phone', '777555999')
+            ->type('mail', 'jan@kowalski.pl')
+            ->click('#doctor-select')
+            ->click('#doctor-select option[value="5"]')
+            ->click('#sendMessageButton')
+            ->assertSee('Ta data jest już zajęta. Wybierz inną datę lub lekarza.');
         });
+
+        $this->assertDatabaseCount('clinic', 1);
+
     }
 }
